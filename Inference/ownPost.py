8fb275d0dc08a7514d38ba4a44630aa1a9413753
Plaetean <pedersenc@cardiff.ac.uk>
2017-03-02 16:59:18 +0100

    Updated my posterior plotting script and added a distance posterior histogram

--- Inference/ownPost.py
+++ Inference/ownPost.py
@@ -2,13 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pycbc.io import InferenceFile
 
-#Iteration to plot
-iteration=4999
-num_walkers=5000
+#Select file and paramters
 parameter="distance"
+folder="20170228-225207/"
+
+#Iteration range to sample
+it_start=9950
+it_end=9999
+num_walkers=5000
+
+#Combine inputs to form variables
+data_name="output.hdf"
+savename=folder+parameter
 
 #Read in file
-datafile="20170228-225207/output.hdf"
+datafile=folder+data_name
+print datafile
 fp = InferenceFile("%s" % datafile, "r")
 
 parameter_values=np.array([])
@@ -16,14 +25,17 @@ parameter_values=np.array([])
 for aa in range(num_walkers):
    samples = fp.read_samples("%s" % parameter, walkers=aa)
    temp=getattr(samples,parameter)
-   temp=temp[::1000]
+   temp=temp[it_start:it_end]
    for bb in range(len(temp)):
       parameter_values=np.append(parameter_values,temp[bb])
 
+values=len(parameter_values)
+
 plt.figure()
-plt.title("%d iterations" % len(parameter_values))
+plt.title("Iterations %d to %d; %d data points" % (it_start,it_end,values))
 plt.hist(parameter_values,50)
 plt.axvline(x=100,linewidth=2,color='r')
 plt.xlabel("%s" % parameter)
+plt.savefig("%s.png" % savename)
 plt.show("hold")
 
