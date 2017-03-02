c3c9ba077f80070ac61bf093c098550d96056bda
Plaetean <pedersenc@cardiff.ac.uk>
2017-03-02 13:21:01 +0100

    Fixed bug

--- Inference/ownPost.py
+++ Inference/ownPost.py
@@ -3,7 +3,6 @@ import numpy as np
 from pycbc.io import InferenceFile
 
 datafile="20170228-225207/output.hdf"
-parameter=mchirp
 
 fp = InferenceFile("%s" % datafile, "r")
 samples = fp.read_samples("mchirp", walkers=0)
