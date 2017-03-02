81938c4620b3cb610c127b911affbaf2bedc427c
Plaetean <pedersenc@cardiff.ac.uk>
2017-03-02 12:41:04 +0000

    Updated ownPost to plot a histogram for a given iteration - will allow me to compare with the plotPost.sh output

--- Inference/ownPost.py
+++ Inference/ownPost.py
@@ -2,10 +2,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pycbc.io import InferenceFile
 
-datafile="20170228-225207/output.hdf"
+#Iteration to plot
+iteration=4999
+num_walkers=5000
 
+#Read in file
+datafile="20170228-225207/output.hdf"
 fp = InferenceFile("%s" % datafile, "r")
-samples = fp.read_samples("mchirp", walkers=0)
-print samples.mchirp
-print len(samples.mchirp)
+
+parameter_values=np.zeros(num_walkers)
+
+for aa in range(num_walkers):
+   samples = fp.read_samples("mchirp", walkers=0)
+   temp=samples.mchirp
+   parameter_values[aa]=temp[iteration]
+
+plt.figure()
+plt.hist(parameter_values,200)
+plt.xlabel("Chirp mass")
+plt.show("hold")
 
