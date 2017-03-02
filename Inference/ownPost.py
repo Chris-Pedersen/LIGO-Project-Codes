8fdd36949de6405b00fce9cbfa57dc94c26447bd
Plaetean <pedersenc@cardiff.ac.uk>
2017-03-02 16:16:56 +0100

    Generalised

--- Inference/ownPost.py
+++ Inference/ownPost.py
@@ -5,22 +5,25 @@ from pycbc.io import InferenceFile
 #Iteration to plot
 iteration=4999
 num_walkers=5000
-parameter="mchirp"
+parameter="distance"
 
 #Read in file
 datafile="20170228-225207/output.hdf"
 fp = InferenceFile("%s" % datafile, "r")
 
-parameter_values=np.zeros(num_walkers)
+parameter_values=np.array([])
 
 for aa in range(num_walkers):
    samples = fp.read_samples("%s" % parameter, walkers=aa)
    temp=getattr(samples,parameter)
-   parameter_values[aa]=temp[iteration]
+   temp=temp[::1000]
+   for bb in range(len(temp)):
+      parameter_values=np.append(parameter_values,temp[bb])
 
-print parameter_values
 plt.figure()
+plt.title("%d iterations" % len(parameter_values))
 plt.hist(parameter_values,50)
-plt.xlabel("Chirp mass")
+plt.axvline(x=100,linewidth=2,color='r')
+plt.xlabel("%s" % parameter)
 plt.show("hold")
 
