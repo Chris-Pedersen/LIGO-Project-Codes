05d5c9aea09f9b821409305ec110ced34a90ad08
Plaetean <pedersenc@cardiff.ac.uk>
2017-03-02 11:41:04 +0000

    Added a posterior plotting function

---
+++ Inference/posterior.sh
@@ -0,0 +1,12 @@
+TARGETDIR=20170228-225207
+ITER=4999
+INPUT_FILE={$TARGET}/output.hdf
+OUTPUT_FILE=${TARGET}/scatter.png
+pycbc_inference_plot_posterior \
+    --iteration ${ITER} \
+    --input-file ${INPUT_FILE} \
+    --output-file ${OUTPUT_FILE} \
+    --plot-scatter \
+    --plot-marginal \
+    --z-arg logplr \
+    --parameters "ra" \
