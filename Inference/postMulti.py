081bf3ec738c72a93a3e843f29a8994ff8b0d74c
Plaetean <pedersenc@cardiff.ac.uk>
2017-03-04 14:42:31 +0000

    Starting to automate posterior plotting more

---
+++ Inference/postMulti.py
@@ -0,0 +1,41 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+from pycbc.io import InferenceFile
+
+#Select file and paramters
+parameter="inclination"
+folder="20170228-225207/"
+injected_value=1.57
+
+#Walker number - should be sticking with 5000
+num_walkers=5000
+
+#Combine inputs to form variables
+data_name="output.hdf"
+savename=folder+parameter
+
+#Read in file
+datafile=folder+data_name
+fp = InferenceFile("%s" % datafile, "r")
+
+def plostPosterior(parameter,inj):
+   #Take last iteration of each walker
+   parameter_values=np.array([])
+   for aa in range(num_walkers):
+      samples = fp.read_samples("%s" % parameter, walkers=aa)
+      temp=getattr(samples,parameter)
+      parameter_values=np.append(parameter_values,temp[-1])
+   values=len(parameter_values)
+   #Plot and save
+   plt.figure()
+   plt.title("%d data points" % (values))
+   plt.hist(parameter_values,50)
+   plt.axvline(x=inj,linewidth=2,color='r')
+   plt.xlabel("%s" % parameter)
+   plt.savefig("%s.png" % savename)
+   plt.show("hold")
+   print "Plot saved as %s.png" % savename
+
+print "DONE"
