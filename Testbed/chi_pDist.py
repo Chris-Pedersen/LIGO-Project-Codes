4cc50e99d6d12d56f49b8e66854dc268be83254c
Plaetean <pedersenc@cardiff.ac.uk>
2017-03-30 19:45:18 +0100

    Added plot function

--- Testbed/chi_pDist.py
+++ Testbed/chi_pDist.py
@@ -64,33 +64,17 @@ def chi_prec(q,mass1,mass2,s1_a,s1_polar,s2_a,s2_polar):
 chi_p=chi_prec(q,m1,m2,s1_a,s1_polar,s1_a,s2_polar)
 
 ## Plot chi_p and component masses for sanity
-print chi_p[1:10]
-
-## Save data
-
-'''
-# We can make pairs of distributions together, instead of apart.
-two_mass_distributions = distributions.Uniform(mass3=(1.6, 3.0),
-                                               mass4=(1.6, 3.0))
-two_mass_samples = two_mass_distributions.rvs(size=1000000)
-
-# Choose 50 bins for the histogram subplots.
-n_bins = 50
-
-# Plot histograms of samples in subplots
 fig, axes = plt.subplots(nrows=2, ncols=2)
 ax0, ax1, ax2, ax3, = axes.flat
-
-ax0.hist(mass1_samples['mass1'], bins = n_bins)
-ax1.hist(mass2_samples['mass2'], bins = n_bins)
-ax2.hist(two_mass_samples['mass3'], bins = n_bins)
-ax3.hist(two_mass_samples['mass4'], bins = n_bins)
-
-ax0.set_title('Mass 1 samples')
-ax1.set_title('Mass 2 samples')
-ax2.set_title('Mass 3 samples')
-ax3.set_title('Mass 4 samples')
-
+ax0.hist(m1, bins = n_bins)
+ax1.hist(m2, bins = n_bins)
+ax2.hist(chi_p, bins = n_bins)
+ax3.hist(q, bins = n_bins)
+ax0.set_title('Mass 1')
+ax1.set_title('Mass 2')
+ax2.set_title('chi_p')
+ax3.set_title('q')
 plt.tight_layout()
-plt.savefig("distributions.png")
-'''
+plt.savefig("priors.png")
+
+## Save data
