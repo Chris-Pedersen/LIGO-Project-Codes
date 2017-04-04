9c8c45ab78b8abc6da2739d6e4dbedf23eb4ca58
Plaetean <pedersenc@cardiff.ac.uk>
2017-04-04 19:43:09 +0200

    Adding priors to chi_p and chi_eff

--- Inference/posteriors.py
+++ Inference/posteriors.py
@@ -244,6 +244,13 @@ def plotPosterior(parameter):
    plt.axvline(x=upper_90,linewidth=2,linestyle='dashed',color='k')
    plt.xlabel("%s" % parameter)
    plt.grid()
+   ## Plot priors for derived spin parameters
+   if parameter=="chi_p":
+      prior=np.loadtxt("priors/chi_p_prior.txt")
+      plt.hist(prior,50,normed=True,alpha=0.6)
+   elif parameter=="chi_eff":
+      prior=np.loadtxt("priors/chi_eff_prior.txt")
+      plt.hist(prior,50,normed=True,alpha=0.6)
    plt.savefig("%s.png" % savename)
    print "Plot saved as %s.png" % savename
 
