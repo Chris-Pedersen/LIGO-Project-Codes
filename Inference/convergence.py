82185e2f7343bc2262b52408743056cef93d0507
Plaetean <pedersenc@cardiff.ac.uk>
2017-04-06 12:44:49 +0200

    Convergence test

--- Inference/convergence.py
+++ Inference/convergence.py
@@ -15,6 +15,7 @@ folder="jobs/"+folder+"/"
 ## Combine inputs to form variables
 data_name="output.hdf"
 savename="convergences/convtest.png"
+num_walkers=5000
 
 ## Function to extract posterior for a given parameter
 ## at a given iteration
@@ -35,12 +36,15 @@ def getParameter(parameter,iteration):
 def findMeans(parameter):
    means=np.zeros(11)
    for aa in range(11): ## Loop over iterations from start to finish
+      print 100*aa/11
       lower=getParameter(parameter,aa) ## Earlier iteration
       upper=getParameter(parameter,aa+1) ## Later iteration
       lower=np.mean(lower) ## Mean of earlier it
       upper=np.mean(upper) ## Mean of later it
       means[aa]=abs(lower-upper)/lower ## Relative change in mean
    return means
+
+means=findMeans(parameter)
       
 ## Plot results
 xaxis=np.linspace(1,12,11)
