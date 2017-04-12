b9eaa706768f6ef789521aed1b90bfcc2b30196c
Plaetean <pedersenc@cardiff.ac.uk>
2017-04-12 15:48:57 +0100

    Injection sims working great

--- Precession/det_response.py
+++ Precession/det_response.py
@@ -1,38 +1,24 @@
-import h5py
-import copy
-import argparse
-from pycbc import DYN_RANGE_FAC, pnutils
-from pycbc.io import InferenceFile
-from pycbc.inference import option_utils
-from pycbc.types import TimeSeries, FrequencySeries
-from pycbc import strain as pystrain
-from pycbc import waveform
-import numpy
-import matplotlib
-matplotlib.use('Agg')
 from matplotlib import pyplot
-from pycbc.filter import match
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--injection-file')
-parser.add_argument('--output-file', required=True)
-opts = parser.parse_args()
 
-        
-colors = {'H1': 'r', 'L1': 'g'}
-for ifo in ['H1', 'L1']:
-   injf = inject.InjectionSet(opts.injection_file)
-   ti = injf.make_strain_from_inj_object(injf.table[0], wh_strain.delta_t, ifo, f_lower=gen.current_params['f_lower'])
-   fi = ti.to_frequencyseries(delta_f=gen.current_params['delta_f'])
-   if len(fi) < len(psd):
-      fi.resize(len(psd))
-   elif len(psd) < len(fi):
-      fi = fi[:len(psd)]
-   fi /= asd
-   ti = fi.to_timeseries()
-   ax.plot(ti.sample_times.numpy()-gps_time, ti.data, 'b-', lw=2, zorder=2)
-   #ax.set_xlim(xmin, xmax)
-   #ax.set_ylim(ylim)
-   ax.set_ylabel('{} whitened strain'.format(ifo))
-   #if ii == 2:
-   #   ax.set_xlabel('GPS time - {} (s)'.format(gps_time))
+
+injection_file_name="injection.xml.gz"
+ii=0
+fig = pyplot.figure()
+
+xmin=-0.5
+xmax=0.05
+
+for ifo in ['H1', 'L1', 'V1', 'I1', 'K1']:
+   ii+=1
+   delta_t=1./2048
+   fmin=20
+   from pycbc import inject
+   injf = inject.InjectionSet(injection_file_name)
+   h = injf.make_strain_from_inj_object(injf.table[0], delta_t, ifo, f_lower=fmin)
+   ax = fig.add_subplot(5,1,ii)
+   ax.set_xlim(xmin, xmax)
+   ax.plot(h.sample_times.numpy(), h)
+   pyplot.title("%s" % ifo)
+   #pyplot.xlim(-0.5,0.05)
+pyplot.show("hold")
---
+++ Precession/simStrain.sh
@@ -0,0 +1,72 @@
+# define coalescence time, observed masses, and waveform parameters
+TRIGGER_TIME=1126259462.0
+INJ_APPROX=IMRPhenomPv2threePointFivePN
+MASS1=50.
+MASS2=15.
+RA=2.21535724066
+DEC=-1.23649695537
+THETA_JN=1.25
+## Inclination calculated manually currently using findTheta.py
+INC=2.7
+COA_PHASE=1.5
+POLARIZATION=0.8
+DISTANCE=200000 # in kpc
+INJ_F_MIN=20.
+TAPER="start"
+
+# Spin parameters
+MIN_SPIN1=0.9
+MAX_SPIN1=0.9
+MIN_KAPPA1=0.0
+MAX_KAPPA1=0.0
+MIN_SPIN2=0.0
+MAX_SPIN2=0.0
+MIN_KAPPA2=0.9
+MAX_KAPPA2=0.9
+
+# path of injection file that will be created in the example
+INJ_PATH=injection.xml.gz
+
+
+# lalapps_inspinj requires degrees on the command line
+LONGITUDE=`python -c "import numpy; print ${RA} * 180/numpy.pi"`
+LATITUDE=`python -c "import numpy; print ${DEC} * 180/numpy.pi"`
+INC_inj=`python -c "import numpy; print ${INC} * 180/numpy.pi"`
+POLARIZATION_inj=`python -c "import numpy; print ${POLARIZATION} * 180/numpy.pi"`
+COA_PHASE_inj=`python -c "import numpy; print ${COA_PHASE} * 180/numpy.pi"`
+
+# create injection file
+lalapps_inspinj \
+    --output ${INJ_PATH} \
+    --seed 1000 \
+    --f-lower ${INJ_F_MIN} \
+    --waveform ${INJ_APPROX} \
+    --amp-order 7 \
+    --gps-start-time ${TRIGGER_TIME} \
+    --gps-end-time ${TRIGGER_TIME} \
+    --time-step 1 \
+    --t-distr fixed \
+    --l-distr fixed \
+    --longitude ${LONGITUDE} \
+    --latitude ${LATITUDE} \
+    --d-distr uniform \
+    --min-distance ${DISTANCE} \
+    --max-distance ${DISTANCE} \
+    --i-distr fixed \
+    --fixed-inc ${INC} \
+    --coa-phase-distr fixed \
+    --fixed-coa-phase ${COA_PHASE} \
+    --polarization ${POLARIZATION} \
+    --m-distr fixMasses \
+    --fixed-mass1 ${MASS1} \
+    --fixed-mass2 ${MASS2} \
+    --taper-injection ${TAPER} \
+    --enable-spin \
+    --min-spin1 ${MIN_SPIN1} \
+    --max-spin1 ${MAX_SPIN1} \
+    --min-spin2 ${MIN_SPIN2} \
+    --max-spin2 ${MAX_SPIN2} \
+    --min-kappa1 ${MIN_KAPPA1} \
+    --max-kappa1 ${MAX_KAPPA1} \
+
+python det_response.py
