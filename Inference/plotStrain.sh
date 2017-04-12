a7c866d544381b6b025bfecfb9647c8069c41c9a
Plaetean <pedersenc@cardiff.ac.uk>
2017-04-12 17:28:59 +0200

    A load of strain plots

--- Inference/plotStrain.sh
+++ Inference/plotStrain.sh
@@ -5,9 +5,33 @@ RUN=$1
 
 python whitened.py --input-file jobs/${RUN}/output.hdf \
                    --plot-map-waveforms \
+                   --min-xlim -2.5 \
+                   --injection-file jobs/${RUN}/injection.xml.gz \
+                   --output-file jobs/${RUN}/strain_plot25
+
+python whitened.py --input-file jobs/${RUN}/output.hdf \
+                   --plot-map-waveforms \
                    --min-xlim -1.5 \
                    --injection-file jobs/${RUN}/injection.xml.gz \
-                   --output-file jobs/${RUN}/strain_plot
+                   --output-file jobs/${RUN}/strain_plot15
+
+python whitened.py --input-file jobs/${RUN}/output.hdf \
+                   --plot-map-waveforms \
+                   --min-xlim -1.0 \
+                   --injection-file jobs/${RUN}/injection.xml.gz \
+                   --output-file jobs/${RUN}/strain_plot10
+
+python whitened.py --input-file jobs/${RUN}/output.hdf \
+                   --plot-map-waveforms \
+                   --min-xlim -0.5 \
+                   --injection-file jobs/${RUN}/injection.xml.gz \
+                   --output-file jobs/${RUN}/strain_plot05
+
+python whitened.py --input-file jobs/${RUN}/output.hdf \
+                   --plot-map-waveforms \
+                   --min-xlim -0.2 \
+                   --injection-file jobs/${RUN}/injection.xml.gz \
+                   --output-file jobs/${RUN}/strain_plot02
 
 # Rename dictionary
 mv jobs/${RUN}/strain_plot_dic.npy jobs/${RUN}/mapDic.npy
