c95d8c54e2881217a1c16d3b485ec16e33ddefa6
Plaetean <pedersenc@cardiff.ac.uk>
2017-04-28 17:09:47 +0100

    Multi phase should be done now

--- Matching/2D_data/multiPhase.py
+++ Matching/2D_data/multiPhase.py
@@ -5,11 +5,16 @@ import pickle
 import numpy as np
 
 fname="phase_s20psi1"
-fname2="phase_s20psi4"
+fname2="phase_s20psi2"
+fname3="phase_s20psi3"
+fname4="phase_s20psi4"
+
 
 #Data saved in the form of x values, y values, z values
 x,y,z,specs=pickle.load(open("%s.p" % fname,"rb"))
-x2,y,z,specs=pickle.load(open("%s.p" % fname,"rb"))
+x2,y,z,specs=pickle.load(open("%s.p" % fname2,"rb"))
+x3,y,z,specs=pickle.load(open("%s.p" % fname3,"rb"))
+x4,y,z,specs=pickle.load(open("%s.p" % fname4,"rb"))
 m1=specs[0]
 m2=specs[1]
 
@@ -17,18 +22,19 @@ plt.figure()
 plt.subplot(2,2,1)
 plt.contourf(x,y,z,200,cmap="inferno")
 plt.colorbar()
+plt.ylabel("Phase")
 plt.subplot(2,2,2)
 plt.contourf(x2,y,z,200,cmap="inferno")
 plt.colorbar()
 plt.subplot(2,2,3)
-plt.contourf(x2,y,z,200,cmap="inferno")
+plt.contourf(x3,y,z,200,cmap="inferno")
 plt.colorbar()
+plt.xlabel("Inclination")
+plt.ylabel("Phase")
 plt.subplot(2,2,4)
-plt.contourf(x2,y,z,200,cmap="inferno")
+plt.contourf(x4,y,z,200,cmap="inferno")
 plt.colorbar()
-plt.ylabel("phase")
-plt.xlabel("inclination")
-plt.title("m1=%s, approx=%s" % (m1,specs[2]))
+plt.xlabel("Inclination")
 plt.show("hold")
 plt.savefig("multi.png")
 print "DONE"
