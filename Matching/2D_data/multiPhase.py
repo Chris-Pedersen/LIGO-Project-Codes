712d97ec8ea0258d4a67d2a5c380eee5b5c087a4
Plaetean <pedersenc@cardiff.ac.uk>
2017-04-28 17:03:18 +0100

    Multi plots

---
+++ Matching/2D_data/multiPhase.py
@@ -0,0 +1,28 @@
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+
+fname="phase_s20psi1"
+fname2="phase_s20psi4"
+
+#Data saved in the form of x values, y values, z values
+x,y,z,specs=pickle.load(open("%s.p" % fname,"rb"))
+x2,y,z,specs=pickle.load(open("%s.p" % fname,"rb"))
+m1=specs[0]
+m2=specs[1]
+
+plt.figure()
+plt.subplot(1,2,1)
+plt.contourf(x,y,z,200,cmap="inferno")
+plt.colorbar()
+plt.subplot(1,2,2)
+plt.contourf(x2,y,z,200,cmap="inferno")
+plt.colorbar()
+plt.ylabel("phase")
+plt.xlabel("inclination")
+plt.title("m1=%s, approx=%s" % (m1,specs[2]))
+plt.show("hold")
+plt.savefig("multi.png")
+print "DONE"
