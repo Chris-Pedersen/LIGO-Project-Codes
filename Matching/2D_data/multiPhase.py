e5c226e0c8dda119623a4a4276362743b5202ee9
Plaetean <pedersenc@cardiff.ac.uk>
2017-04-28 17:11:50 +0100

    Was plotting wrong data  Please enter the commit message for your changes. Lines starting

--- Matching/2D_data/multiPhase.py
+++ Matching/2D_data/multiPhase.py
@@ -12,9 +12,9 @@ fname4="phase_s20psi4"
 
 #Data saved in the form of x values, y values, z values
 x,y,z,specs=pickle.load(open("%s.p" % fname,"rb"))
-x2,y,z,specs=pickle.load(open("%s.p" % fname2,"rb"))
-x3,y,z,specs=pickle.load(open("%s.p" % fname3,"rb"))
-x4,y,z,specs=pickle.load(open("%s.p" % fname4,"rb"))
+x2,y,z2,specs=pickle.load(open("%s.p" % fname2,"rb"))
+x3,y,z3,specs=pickle.load(open("%s.p" % fname3,"rb"))
+x4,y,z4,specs=pickle.load(open("%s.p" % fname4,"rb"))
 m1=specs[0]
 m2=specs[1]
 
@@ -24,15 +24,15 @@ plt.contourf(x,y,z,200,cmap="inferno")
 plt.colorbar()
 plt.ylabel("Phase")
 plt.subplot(2,2,2)
-plt.contourf(x2,y,z,200,cmap="inferno")
+plt.contourf(x2,y,z2,200,cmap="inferno")
 plt.colorbar()
 plt.subplot(2,2,3)
-plt.contourf(x3,y,z,200,cmap="inferno")
+plt.contourf(x3,y,z3,200,cmap="inferno")
 plt.colorbar()
 plt.xlabel("Inclination")
 plt.ylabel("Phase")
 plt.subplot(2,2,4)
-plt.contourf(x4,y,z,200,cmap="inferno")
+plt.contourf(x4,y,z4,200,cmap="inferno")
 plt.colorbar()
 plt.xlabel("Inclination")
 plt.show("hold")
