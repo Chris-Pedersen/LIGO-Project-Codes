85c26a23c59fdbb703581aeabdd61dc9e478e5b1
Plaetean <pedersenc@cardiff.ac.uk>
2017-05-04 18:35:14 +0200

    Virgo plot for paper

--- Precession/det_response.py
+++ Precession/det_response.py
@@ -1,25 +1,25 @@
-import matplotlib
-matplotlib.use('Agg')
 from matplotlib import pyplot
 
 injection_file_name="injection.xml.gz"
 ii=0
 fig = pyplot.figure()
 
-xmin=-3.05
-xmax=3.05
 
-for ifo in ['H1', 'L1', 'V1']:
+for ifo in ['H1', 'L1', 'V1','I1','K1']:
    ii+=1
    delta_t=1./2048
    fmin=20
    from pycbc import inject
    injf = inject.InjectionSet(injection_file_name)
    h = injf.make_strain_from_inj_object(injf.table[0], delta_t, ifo, f_lower=fmin)
-   ax = fig.add_subplot(3,1,ii)
+   ax = fig.add_subplot(5,1,ii)
    ax.plot(h.sample_times.numpy(), h)
    ax.set_xlim(h.sample_times[int(len(h.sample_times)/2.)], h.sample_times[-1])
    pyplot.title("%s" % ifo)
+   pyplot.ylabel("Strain")
    #pyplot.xlim(xmin,xmax)
 pyplot.xlabel("GPS Time (s)")
+pyplot.tight_layout()
+pyplot.show("hold")
 pyplot.savefig("det_strain.png")
+print "Figure saved as det_strain.png"
